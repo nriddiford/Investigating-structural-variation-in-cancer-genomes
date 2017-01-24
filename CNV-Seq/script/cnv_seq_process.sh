4e0148b9ea5ea137bd6ceccc253a9c679850b5cc
nriddiford <nickriddiford@me.com>
2017-01-24 16:17:57 +0100

    Update

--- CNV-Seq/script/cnv_seq_process.sh
+++ CNV-Seq/script/cnv_seq_process.sh
@@ -4,9 +4,15 @@ Rscript='cnv-seq.R'
 chrom='X'
 
 for file in $@; do
-	stem=$(basename "${file}" )
-	id=$(echo $stem | cut -d'.' -f 1)
-	Rscript $Rscript $file $id $chrom --save
+        stem=$(basename "${file}" )
+        id=$(echo $stem | cut -d'.' -f 1)
+        Rscript $Rscript $file $id $chrom --save
 done
 
 rm Rplots.pdf
+
+if [ ! -d plots ]; then
+    mkdir -p plots;
+fi
+
+mv *pdf plots/
